35e300b8be366f43439a209cc4982acdd9c94167
juandejunin <juandejunin75@gmail.com>
2023-05-05 17:34:08 +0200

    feat: add cors

--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
 const express = require('express')
+const cors = require('cors')
 
 const connection = require('./database/db.js')
 const app = express()
 require('dotenv').config()
 const userRouter = require('./v1/routes/userRouter.js')
+const geolocationRouter = require('./v1/routes/geolocationRouter.js')
 
 const PORT = process.env.PORT
+
+app.use(cors())
 
 app.use(express.json())
 
 app.use('/api/v1/users/', userRouter)
+app.use('/api/v1/geolocation', geolocationRouter)
 app.get('/', (req, res) => {
   res.send("<h2>Let's Go team five!</h2>")
 })
