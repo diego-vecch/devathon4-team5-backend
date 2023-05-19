f7c2036a2640fb5dc127d5822b7108ca04a9a8aa
juandejunin <juandejunin75@gmail.com>
2023-05-19 14:03:29 +0200

    fix: add location

--- src/controllers/mapsController.js
+++ src/controllers/mapsController.js
@@ -1,4 +1,5 @@
 require('dotenv').config()
+const fetch = require('node-fetch')
 
 const KEY_GOOGLE_MAPS = process.env.KEY_GOOGLE_MAPS
 
@@ -19,4 +20,30 @@ const autocomplete = async (req, res, next) => {
   res.status(200).send(description)
 }
 
-module.exports = { autocomplete }
+const maps = async (req, res, next) => {
+  const apiKey = KEY_GOOGLE_MAPS
+  const { address } = req.body
+
+  const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${apiKey}`
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      // Aquí puedes manejar la respuesta de la API
+      console.log(data)
+      const results = data.results[0]
+      return res.send({
+        locality: results.formatted_address,
+        location: results.geometry.location,
+        place_id: results.place_id
+      })
+    })
+    .catch(error => {
+      // Aquí puedes manejar cualquier error que ocurra durante la solicitud
+      console.error(error)
+    })
+
+  
+}
+
+module.exports = { autocomplete, maps }
