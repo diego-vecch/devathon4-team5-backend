d24c0ee8c699cfca25082878faa28d51d871bb83
juandejunin <juandejunin75@gmail.com>
2023-05-24 08:04:34 +0200

    fix: correct spelling accessibility

--- src/controllers/placeController.js
+++ src/controllers/placeController.js
@@ -3,12 +3,11 @@ require('dotenv').config()
 
 const KEY_GOOGLE_MAPS = process.env.KEY_GOOGLE_MAPS
 
-const assessibilityWheelchair = async (req, res, next) => {
+const accessibilityWheelchair = async (req, res, next) => {
   const { place } = req.body
 
   const placeId = async (req, res, nex) => {
     try {
-      // const places = 'conservatoriodemusica'
       const url = `https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=${place}&inputtype=textquery&inputtype=textquery&key=${KEY_GOOGLE_MAPS}`
       const resp = await fetch(url)
       const data = await resp.json()
@@ -20,8 +19,7 @@ const assessibilityWheelchair = async (req, res, next) => {
   }
   const idplaces = placeId()
   const id = await idplaces
-  // console.log(id)
-  const url = `https://maps.googleapis.com/maps/api/place/details/json?place_id=${id}&fields=formatted_address%2Cname%2Copening_hours%2Cwheelchair_accessible_entrance&key=${KEY_GOOGLE_MAPS}`
+  const url = `https://maps.googleapis.com/maps/api/place/details/json?place_id=${id}&key=${KEY_GOOGLE_MAPS}`
 
   https.get(url, (response) => {
     let data = ''
@@ -38,4 +36,4 @@ const assessibilityWheelchair = async (req, res, next) => {
   })
 }
 
-module.exports = { assessibilityWheelchair }
+module.exports = { accessibilityWheelchair }
